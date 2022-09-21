8b0a70a47129820aefc93a93697038cb3c5f982c
brayan200014 <josue200014@outlook.com>
2022-09-20 19:03:07 -0600

    Adding Routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,4 +5,32 @@ router.get('/', (_req, res) => {
   res.json({msg:'Hello World!'});
  });
 
+ router.get('/hello', (req, res) => {
+  const {name = 'NOT NAME FOUND'}= req.query;
+  res.json({name});
+ });
+
+ router.get('/view/:id', (req, res)=> {
+  const { id = -1}= req.params  as unknown as {id?:number};
+  res.json({ id });
+ });
+
+ router.post('/new', (req, res) => {
+  const { name ='NO NAME', email= "", age= 0}= req.body as {name?: String, email?: String, age?: number };
+   res.json({name, email, age});
+
+ });
+
+ router.put('/update/:id', (req, res)=> {
+  const { id = -1}= req.params  as unknown as {id?:number};
+  const { name ='NO NAME', email= "", age= 0}= req.body as {name?: String, email?: String, age?: number };
+  res.json({id, name, email, age});
+
+ });
+
+ router.delete('/delete/:id', (req, res)=> {
+  const { id = -1}= req.params  as unknown as {id?:number};
+  res.json({ id });
+ });
+
 export default router;
