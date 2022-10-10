521995577ddec884d83778c7dcfd918772e57781
brayan200014 <josue200014@outlook.com>
2022-10-09 20:01:26 -0600

    CRUD Users Finished

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,13 @@
 import { Router} from 'express';
 
 import CashFlowRouter  from './CashFlows';
+import UserRouter from './Users';
 
 const router  = Router();
 
 // http://localhost:3001/cashflow/byindex/1
 router.use('/cashflow', CashFlowRouter);
+
+router.use('/user', UserRouter);
 
 export default router;
